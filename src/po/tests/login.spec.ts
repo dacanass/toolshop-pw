abc7fcc2b49c937e49230c45e10c4ceed415f3c8
David Cañas <dacanass@gmail.com>
2025-10-02 08:59:13 -0500

    Enhance login functionality with error message handling and user data management

--- src/po/tests/login.spec.ts
+++ src/po/tests/login.spec.ts
@@ -1,9 +1,21 @@
 import { test, expect } from "./fixtures";
+import { errorMessage, userData } from "../utils/data";
 
 test.describe('Testing Login feature', async () => {
-  test('Should login succesfuly', async ({ page, loginPage }) => {
+  const { email, password } = userData
+
+  test.beforeEach(async ({ loginPage }) => {
     await loginPage.goto()
-    await loginPage.login("customer@practicesoftwaretesting.com", "pass123")
   })
 
+  test('Should login succesfuly', async ({ page, loginPage }) => {
+    await loginPage.login(`${email[0]}`, `${password}`)
+    expect(page).toHaveURL("https://practicesoftwaretesting.com/account")
+  })
+
+  test('Should not be able to login', async ({ page, loginPage }) => {
+    await loginPage.login(`${email[1]}`, `${password}`)
+    await expect(loginPage.generalErrorMessage).toBeVisible()
+    await expect(loginPage.generalErrorMessage).toContainText(errorMessage.loginInvalidCredentials)
+  })
 })
---
+++ src/po/utils/data.ts
@@ -0,0 +1,12 @@
+export const userData = {
+  email: ["customer@practicesoftwaretesting.com", "invalidEmail@example.com"],
+  password: "welcome01"
+}
+
+export const errorMessage = {
+  loginEmailEmpty: "Email is required",
+  loginEmailInvalidFormat:"Email format is invalid",
+  loginPasswordEmpty:"Password is required",
+  loginPasswordLenght:"Password length is invalid",
+  loginInvalidCredentials:"Invalid email or password"
+}
