f94f19eb5a2f59a1c389ba77ad3fbed1939e2380
David Cañas <dacanass@gmail.com>
2025-10-02 11:31:13 -0500

    Refactor login tests for consistency and clarity in error handling

--- src/po/tests/login.spec.ts
+++ src/po/tests/login.spec.ts
@@ -1,21 +1,42 @@
 import { test, expect } from "./fixtures";
 import { errorMessage, userData } from "../utils/data";
 
-test.describe('Testing Login feature', async () => {
+test.describe('Login Feature', async () => {
   const { email, password } = userData
 
   test.beforeEach(async ({ loginPage }) => {
     await loginPage.goto()
   })
 
-  test('Should login succesfuly', async ({ page, loginPage }) => {
+  test('should succesfuly login with valid credentials', async ({ page, loginPage }) => {
     await loginPage.login(`${email[0]}`, `${password}`)
-    expect(page).toHaveURL("https://practicesoftwaretesting.com/account")
+    await expect(page).toHaveURL("https://practicesoftwaretesting.com/account")
   })
 
-  test('Should not be able to login', async ({ page, loginPage }) => {
+  test('should display error message with invalid credentials', async ({ loginPage }) => {
     await loginPage.login(`${email[1]}`, `${password}`)
     await expect(loginPage.generalErrorMessage).toBeVisible()
     await expect(loginPage.generalErrorMessage).toContainText(errorMessage.loginInvalidCredentials)
   })
+
+  test('should display error when email field is empty', async ({ loginPage }) => {
+    await loginPage.login("", `${password}`)
+    await expect(loginPage.emailErrorMessage).toContainText(errorMessage.loginEmailEmpty)
+  })
+
+  test('should display error message when email format is invalid ', async ({ loginPage }) => {
+    await loginPage.login("aaa", `${password}`)
+    await expect(loginPage.emailErrorMessage).toContainText(errorMessage.loginEmailInvalidFormat)
+  })
+
+  test('should display error message when password is empty', async ({ loginPage }) => {
+    await loginPage.login(`${email}`, "")
+    await expect(loginPage.passwordErrorMessage).toContainText(errorMessage.loginPasswordEmpty)
+  })
+
+  test('should display error message when password is too short', async ({ loginPage }) => {
+    await loginPage.login(`${email}`, "pa")
+    await expect(loginPage.passwordErrorMessage).toContainText(errorMessage.loginPasswordLenght)
+  })
+
 })
